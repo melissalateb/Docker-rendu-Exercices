1ca7771e1198afbd995f2c8bf5b52eb724b81e2f
midadey <latebmelissa@gmail.com>
2023-11-20 11:28:18 +0100

    Exercice 1 - Création de deux server comuniquant entre eux

---
+++ Rendu Day One/Exercice1/Server1.py
@@ -0,0 +1,49 @@
+# Server 1
+import time
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+SERVER2_URL = 'http://localhost:5372'
+
+@app.route('/pong')
+def ping_pong():
+    try:
+        # Envoie une requête "pong" vers le serveur 2
+        requests.get(f'{SERVER2_URL}/ping', timeout=0.5)
+        return 'Ping successful!'
+    except requests.RequestException as e:
+        return f'Error communicating with Server 2: {str(e)}', 500
+if __name__ == '__main__':
+    app.run(port=4567)
+# from flask import Flask, request
+# import requests
+# import time
+# import threading
+
+# app = Flask(__name__)
+
+# #l'dresse du serveur 2
+# server2_url = "http://localhost:5002/"
+
+# def send_pong():
+#     while True:
+#         time.sleep(0.5)
+#         requests.get(server2_url + "/ping")
+#         print("Sent Pong")
+#         time.sleep(0.5)
+
+# @app.route('/pong', methods=['GET'])
+# def ping():
+#     print("Received Ping")
+#     return 'Pong'
+
+# if __name__ == 'main':
+#     threading.Thread(target=send_pong).start()
+#     app.run(port=5000)  # Exécution du serveur sur le port 5000
+# from flask import Flask, request
+# import requests
+# import time
+# import threading
+
+# app = Flask(__name__)
---
+++ Rendu Day One/Exercice1/Server2.py
@@ -0,0 +1,50 @@
+# Server 2
+from flask import Flask
+import requests
+import time
+
+app = Flask(__name__)
+SERVER1_URL = 'http://localhost:4567'
+
+@app.route('/ping')
+def ping():
+    try:
+        # Attend une demi-seconde avant d'envoyer une requête "pong" vers le serveur 1
+        
+        requests.get(f'{SERVER1_URL}/pong', timeout=0.1)
+        return 'Pong sent!'
+    
+    except requests.RequestException as e:
+        # return 'Error communicating with Server 1', 500
+        return f'Error communicating with Server 1: {str(e)}', 500
+
+if __name__ == '__main__':
+    app.run(port=5372)
+
+# from flask import Flask, request
+# import requests
+# import time
+# import threading
+
+# app = Flask(__name__)
+
+# #l'adresse du serveur 1
+# server1_url = "http://localhost:5000/" 
+
+# def send_ping():
+#     while True:
+#         time.sleep(0.5)
+#         requests.get(server1_url + "/pong")
+#         print("Sent Ping")
+#         time.sleep(0.5)
+
+# @app.route('/ping', methods=['GET'])
+# def ping():
+#     # time.sleep(0.5)
+#     # requests.get(server1_url + "/ping")
+#     print("Received Pong")
+#     return 'Ping'
+
+# if __name__ == 'main':
+#     threading.Thread(target=send_ping).start()
+#     app.run(port=5002)  # Exécution du serveur sur le port 5002
